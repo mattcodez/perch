e7cde2225e492b6682263290732edee3b4426fa2
Matt Molnar <matthew.molnar@gmail.com>
2016-11-19 16:14:41 -0500

    Start reading temperature

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict';
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+
+const DEBUG_MODE = true; //TODO: default to false and set through cmd/env
 
 //consider this unchangeable during program run
+//TODO import from it's own file
 const config = Object.freeze({
   cycleTempChange:       1,   //degrees(C) - How much do we change the termperature
                               //before turning off. Always uses celsius
@@ -22,7 +27,7 @@ const config = Object.freeze({
                               //file path string - location of device file
                               //for reading the temperature
 
-  filePollRate:           10000,
+  filePollRate:           1000,
                               //time(ms) - How often should we check
                               //thermometerFilePath for the temperature
 });
@@ -30,8 +35,16 @@ const config = Object.freeze({
 let currentTempRead;  //Global to hold the current termperature read from
                       //the device file
 
-function getAndSetTemperature(){
-  fs.readFile(config.thermometerFilePath)
+function async getAndSetTemperature(){
+  try{
+    const content = await fs.readFile(config.thermometerFilePath);
+    currentTempRead = content.substr(content.lastIndexOf('t=') + 2) / 1000;
+    DEBUG_MODE && console.log(`currentTempRead set to ${currentTempRead}`);
+  }
+  catch(e){
+    //TODO:stop everything
+    console.error(e);
+  }
 }
 //Continuously poll
 const readInterval = setInterval(getAndSetTemperature, config.filePollRate);
